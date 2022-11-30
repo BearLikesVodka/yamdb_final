0e731f8170f62a1efc27fe144c893c2dfec255f4
Глеб Рыбин <bearlikesvodka@gmail.com>
2022-11-30 14:06:46 +0300

    fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default_secret_key')
